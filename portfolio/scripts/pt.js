9b77603ab1a14ecebd6cd3ebd2283f008b32563d
oheneemmanuel <ohenekwakye24@gmail.com>
2025-12-23 12:43:21 +0000

    working on  my portfolio building the home page

---
+++ portfolio/scripts/pt.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  
+
+    const Button = document.getElementById("menu");
+
+    const menu = document.getElementById("animate-me");
+
+    Button.addEventListener('click', () => {
+        menu.classList.toggle("show");
+        Button.classList.toggle("show");
+    
+    
+    
+    });
+    
+});
